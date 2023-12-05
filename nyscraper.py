5667933d9ecd3daa7d06f83e70da0dbf781c3990
Changmin Andrew Moon <changminmoon@naver.com>
2023-12-05 09:41:09 +0900

    Add files via upload

---
+++ nyscraper.py
@@ -0,0 +1,54 @@
+import requests
+import csv
+import time
+#Manually get Api-Key from the New York Times Developer Portal
+api_key = 'uPe3dHtxZg7i73LYAMybZUJnmGa7KB4k'
+base_url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json'
+#Set Keywords
+keywords = ['online', 'SNS complain', 'digital', 'e-WOM', 'electronic word of mouth',
+            'complaint management', 'complaint handling', 'complaint recovery',
+            'complaint apology', 'complain recovery', 'complain apology', 'complain']
+# Date range (format: YYYYMMDD)
+begin_date = '20220101'
+end_date = '20231127'
+# Save file name 
+csv_file = 'nyt_data.csv'
+#Requsting New York Times api for data
+def make_api_request(keyword):
+    query = keyword
+    params = {
+        'api-key': api_key,
+        'q': query,
+        'begin_date': begin_date,
+        'end_date': end_date
+    }
+    response = requests.get(base_url, params=params)
+    if response.status_code == 200:
+        articles = response.json()['response']['docs']
+        data = []
+        for article in articles:
+            url = article['web_url']
+            headline = article['headline']['main'] if 'headline' in article and 'main' in article['headline'] else ''
+            article_content = article['lead_paragraph'] if 'lead_paragraph' in article else ''
+            article_content = article_content.replace('\n', ' ')
+
+            # Make the comparison case-insensitive
+            if any(keyword_part.lower() in field.lower() for keyword_part in keyword.split() for field in [url, headline, article_content]):
+                data.append([url, headline, article_content])
+
+        return data
+    else:
+        print(f"Error: {response.status_code}")
+        print(response.text)
+        return []
+#Print the data into csv file
+with open(csv_file, 'w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file, quoting=csv.QUOTE_MINIMAL)
+    writer.writerow(['URL', 'Headline', 'Article Content'])
+
+    for keyword in keywords:
+        urls_and_headlines = make_api_request(keyword)
+        writer.writerows(urls_and_headlines)
+        time.sleep(12)
+#Sleeping for 12 seconds due to api rate-limit
+print(f"Scraping completed. Data has been saved to {csv_file}") # Return log
